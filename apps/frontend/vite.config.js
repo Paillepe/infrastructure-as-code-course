24e1f75d1c583efda139126af1f97cf1fa1e36b1
kar <kar_fr_fr@yahoo.fr>
2026-01-07 15:59:47 +0100

    MODIF: vite.conf remanié

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -4,20 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
-    //allowedHosts: [
-    //  '.azurecontainerapps.io'
-    ]
-
-	  //  proxy: {
-  //    '/api': {
-  //      target: 'http://backend-node:3000',
-  //      changeOrigin: true
-  //    },
-  //    '/health': {
-  //      target: 'http://backend-node:3000',
-  //      changeOrigin: true
-  //    }
-    }
-  })
+  build: {
+    outDir: 'dist'
+  }
+})
